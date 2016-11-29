1eb8163afafaf9c979d6ba9157576e0a903f32b3
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-11-29 15:36:36 +0000

    edited format of statment view so it now matches tech test example

--- bankapp.rb
+++ bankapp.rb
@@ -8,28 +8,25 @@ class Account
   end
 
   def transaction(date, type, amount)
-    if type == 'credit'
-      @balance += amount
-    else
-      @balance -= amount
-    end
+    amount.to_f
+    type == 'credit' ? @balance += amount : @balance -= amount
     balance = @balance
-    add_transaction(date: date, type: type, amount: amount, balance: balance)
-  end
-
-  def add_transaction(info)
-    @transactions.unshift(info)
+    @transactions.unshift(date: date, type: type, amount: amount, balance: balance)
   end
 
   def print_header
-    puts "date       || credit   || debit   || balance "
+    puts "date       || credit || debit    || balance "
     puts "---------------------------------------------"
   end
 
   def print_transaction
     print_header
     @transactions.each do |transaction|
-      puts "#{transaction[:date]} || #{transaction[:type]} || £#{transaction[:amount]} || £#{transaction[:balance]}"
+      if transaction[:type] == 'credit'
+        puts "#{transaction[:date]} ||£#{transaction[:amount]}  ||          || £#{transaction[:balance]}"
+      else
+        puts "#{transaction[:date]} ||        ||£#{transaction[:amount]}    || £#{transaction[:balance]}"
+      end
     end
   end
 
