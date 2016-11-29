870373800b7e15a6d12ef68f19777d27c0e8a058
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-11-29 13:43:00 +0000

    changed add_function method so it now means that transactions are listed from newest first

--- bankapp.rb
+++ bankapp.rb
@@ -1,31 +1,36 @@
 class Account
 
-attr_reader :balance, :transactions
+  attr_reader :balance, :transactions
 
   def initialize
     @balance = 0
     @transactions = []
   end
 
-
-def transaction(date, type, amount)
-  if type == 'credit'
-    @balance += amount
-  else
-    @balance -= amount
+  def transaction(date, type, amount)
+    if type == 'credit'
+      @balance += amount
+    else
+      @balance -= amount
+    end
+    balance = @balance
+    add_transaction(date: date, type: type, amount: amount, balance: balance)
   end
-  balance = @balance
-  add_transaction(date: date, type: type, amount: amount, balance: balance)
-end
 
-def add_transaction(info)
-  @transactions << info
-end
-
-def print_transaction
-  @transactions.each do |transaction|
-     puts "Date: #{transaction[:date]} || Type: #{transaction[:type]} || Amount: £#{transaction[:amount]} || Balance £#{transaction[:balance]}".center(100)
+  def add_transaction(info)
+    @transactions.unshift(info)
   end
-end
+
+  def print_header
+    puts "date       || credit   || debit   || balance "
+    puts "---------------------------------------------"
+  end
+
+  def print_transaction
+    print_header
+    @transactions.each do |transaction|
+      puts "#{transaction[:date]} || #{transaction[:type]} || £#{transaction[:amount]} || £#{transaction[:balance]}"
+    end
+  end
 
 end
--- spec/bankapp_spec.rb
+++ spec/bankapp_spec.rb
@@ -12,14 +12,14 @@ describe Account do
 
   context 'ability to make a deposit' do
     it 'should allow a customer to make a deposit and return new balance' do
-      account.transaction('14/02/12', 'credit', 300)
+      account.transaction('14/02/2012', 'credit', 300)
       expect(account.balance).to eq 300
     end
   end
 
   context 'ability to make a withdrawal' do
     it 'should allow a customer to make a withdrawal and return new balance' do
-      account.transaction('14/03/12', 'debit', 300)
+      account.transaction('14/03/2012', 'debit', 300)
       expect(account.balance).to eq 0
     end
   end
@@ -27,8 +27,8 @@ describe Account do
   context 'ability to make a deposit and specify the date this was done' do
     it 'should allow a customer to make a deposit and enter the date completed' do
       account = Account.new
-      account.transaction('14/01/12', 'credit', 100)
-      expect(account.transactions).to include date: '14/01/12', type: 'credit', amount: 100, balance: 100
+      account.transaction('14/01/2012', 'credit', 100)
+      expect(account.transactions).to include date: '14/01/2012', type: 'credit', amount: 100, balance: 100
     end
   end
 end
@@ -38,18 +38,18 @@ describe 'statement' do
   context 'customers want to view their transactions' do
     it 'should allow customers to see a transaction' do
       account = Account.new
-      account.transaction('16/05/12', 'credit', 300)
-      expect(account.print_transaction[0]).to eq({:date=>"16/05/12", :type=>"credit", :amount=>300, :balance=>300})
-      end
+      account.transaction('16/05/2012', 'credit', 300)
+      expect(account.print_transaction[0]).to eq({:date=>"16/05/2012", :type=>"credit", :amount=>300, :balance=>300})
     end
-
-    context 'customers want to view their transactions' do
-      it 'should allow customers to see all their transactions' do
-        account = Account.new
-        account.transaction('14/02/12', 'credit', 300)
-        account.transaction('14/03/12', 'debit', 300)
-        account.transaction('16/05/12', 'credit', 300)
-        expect(account.print_transaction).to eq( [{:date=>"14/02/12", :type=>"credit", :amount=>300, :balance=>300}, {:date=>"14/03/12", :type=>"debit", :amount=>300, :balance=>0}, {:date=>"16/05/12", :type=>"credit", :amount=>300, :balance=>300}])
-        end
-      end
   end
+
+  context 'customers want to view their transactions' do
+    it 'should allow customers to see all their transactions' do
+      account = Account.new
+      account.transaction('14/01/2012', 'credit', 100)
+      account.transaction('16/05/2012', 'credit', 300)
+      expect(account.print_transaction).to eq([{:date=>"16/05/2012", :type=>"credit", :amount=>300, :balance=>400}, {:date=>"14/01/2012", :type=>"credit", :amount=>100, :balance=>100}])
+    end
+  end
+
+end
